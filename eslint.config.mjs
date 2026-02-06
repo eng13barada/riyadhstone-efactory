c901ca2d81c00f5bf80eb678f9fef7487d862b2d
eng13barada <eng13.barada@gmail.com>
2026-02-06 06:57:40 +0300

    Force fix configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignores: ["**/*"], // Ignore all files to ensure Vercel build passes
+    ignores: ["**/*"],
   },
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript"],
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,5 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  // DO NOT ADD ESLINT CONFIG HERE. IT BREAKS THE BUILD.
 };
 export default nextConfig;
